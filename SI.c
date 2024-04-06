e3d94a91e92ecf344fd543b86009a58b44dc6e43
Raksha Adhikari <156924801+Thisismygirlrakshaforsure@users.noreply.github.com>
2024-04-06 06:48:49 +0545

    Create SI.c

---
+++ SI.c
@@ -0,0 +1,17 @@
+WAP to calculate simple interest using function
+#include 
+float interest(void); //function declaration
+int main()
+{   float si;
+    si=interest(); //function call
+    printf("Simple interst is %.2f\n",si);
+    return 0;
+}
+float interest() //function definition
+{
+    float p,t,r,i;
+    printf("Enter Principal, Time and Rate");
+    scanf("%f%f%f",&p,&t,&r);
+    i=(p*t*r)/100;
+    return i; //function return value
+}
